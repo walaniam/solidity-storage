14c553fca8215ee4462812d0d3f783471fd9bd81
Mariusz Walania <mariusz.walania@gmail.com>
2021-12-01 21:23:36 +0100

    .env location

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,11 @@
+const os = require("os");
 const path = require("path");
 const fs = require('fs');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const dotenv = require('dotenv');
 
 function loadEnv(envFile) {
-  const envPath = __dirname + '/../' + envFile;
+  const envPath = os.homedir() + '/.ethereum/' + envFile;
   const envPathLocalDir = __dirname + '/' + envFile;
   if (fs.existsSync(envPathLocalDir)) {
     console.log("Loading .env from: " + envPathLocalDir);
@@ -21,7 +22,7 @@ loadEnv('.env_ropsten');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/public/contracts"),
+  //contracts_build_directory: path.join(__dirname, "client/public/contracts"),
   networks: {
     develop: {
       host: "127.0.0.1",
